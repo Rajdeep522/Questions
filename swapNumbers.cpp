eb4274d74d056e87535b9eb1af4045566c73ea54
Rajdeep Debnath <rajdeepdebnath522@gmail.com>
2024-08-30 00:13:55 +0530

    add question

--- swapNumbers.cpp
+++ swapNumbers.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
 using namespace std;
-
+//Swap two number without using thired variable 
 int main(){
-
+    int a,b;
+    cout<<"Enter a= ";
+    cin>>a;
+    cout<<"Enter b= ";
+    cin>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    // b=a+b;
+    cout<<"a= "<<a<<endl;
+    cout<<"b= "<<b<<endl;
     return 0;
 }
