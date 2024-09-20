561bf85d05f6afc9e251aaa85734411ff42b6993
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-09-20 22:17:25 +0530

    Update BinrToDec.cpp

--- BinrToDec.cpp
+++ BinrToDec.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
+//Decimal to Binary conversion
 int BinrToDec(int n){
-  
+    int bin=0;
+    int pow=1;//10^0
+    while (n>0){
+        int rem=n%2;
+        n=n/2;
+        bin+=rem*pow;
+        pow*=10;
+    }
+    return bin;
+    
 }
 int main(){
-  
-return 0;
+    int n;
+    cin>>n;
+    cout<<BinrToDec(n)<<endl;
+
+    return 0;
 }
