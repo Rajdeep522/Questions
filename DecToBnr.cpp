5555735b982f490c6b3e321e155d044e8c09ca22
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-09-21 07:38:04 +0530

    Update DecToBnr.cpp

--- DecToBnr.cpp
+++ DecToBnr.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
+//Binary to Decimal conversion
+int BnrToDec(int n){
+    int dec=0;
+    int pow=1;//2**0
+    while (n>0) {
+        int rem=n%10;
+        n=n/10;
+        dec+=rem*pow;
+        pow*=2;
 
+    }
+    return dec;
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<BnrToDec(n)<<endl;
+    return 0;
+}
