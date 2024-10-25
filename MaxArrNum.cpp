738744a499b9ea20edad583d0104dc9814ca8161
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-10-25 20:46:09 +0530

    Update MaxArrNum.cpp

--- MaxArrNum.cpp
+++ MaxArrNum.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+// Find maximum value in the array
+int main(){
+    
+    int arr[]={10,20,-5,62,-20,100};
+    int greatest=INT_MIN;
+    for (int i = 0; i <= 5; i++){
+        if (arr[i] > greatest){
+            greatest=arr[i];
+        }
+        // greatest=max(arr[i],greatest);
+        
+    }
+    cout<<"Greatest is : "<<greatest<<endl;
 
+}
