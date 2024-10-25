e501ea7a10f70aa37dcf6e7e324bff9bc70ff99a
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-10-25 20:43:56 +0530

    Update MinArrNum.cpp

--- MinArrNum.cpp
+++ MinArrNum.cpp
@@ -1,5 +1,18 @@
-#include<iostream.h>
+#include<iostream>
+#include<climits>
 using namespace std;
+// Find minimum value in the array
 int main(){
-  return 0;
+    
+    int arr[]={10,20,-5,62,-20,100};
+    int smallest=INT_MAX;
+    for (int i = 0; i <= 5; i++){
+        // if (arr[i] < smallest){
+        //     smallest=arr[i];
+        // }
+        smallest=min(arr[i],smallest);
+        
+    }
+    cout<<"Smallest is : "<<smallest<<endl;
+
 }
