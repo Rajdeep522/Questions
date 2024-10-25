aba4de288c386a14b0dfede447de2622f119f42a
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-10-25 20:45:35 +0530

    Update MinArrNum.cpp

--- MinArrNum.cpp
+++ MinArrNum.cpp
@@ -7,12 +7,16 @@ int main(){
     int arr[]={10,20,-5,62,-20,100};
     int smallest=INT_MAX;
     for (int i = 0; i <= 5; i++){
-        // if (arr[i] < smallest){
-        //     smallest=arr[i];
-        // }
+                                                                            // if (arr[i] < smallest){
+                                                                            //     smallest=arr[i];             <<<- `ANOTHER LOGIC`
+                                                                            // }
         smallest=min(arr[i],smallest);
         
     }
     cout<<"Smallest is : "<<smallest<<endl;
 
 }
+
+
+
+
