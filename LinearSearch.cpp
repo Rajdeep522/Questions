50a75657d066d94a0cc6b2b4ffd8af6dca662994
Rajdeep Debnath <158467163+Rajdeep522@users.noreply.github.com>
2024-10-26 20:44:03 +0530

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+//Linear Search 
 
+int Search(int arr[],int size,int target){
+    for (int i = 0; i < size; i++){
+        if (arr[i]==target){
+            return i;  // return index of the element
+        }   
+    }
+    return -1; //When invalid 
+    
+}
+int main(){
+    int arr[]={4,2,7,8,1,2,1};
+    int size=7;
+    int target=80;
+    cout<<Search(arr,size,target);
+   return 0;
+}
